9b7d0a9d389f52364c3db918e0188f2ec8b91354
daisof06-git <daisof06@gmail.com>
2026-08-02 22:17:34 -0300

    Update 263A - Beautiful Matrix.cpp

--- 800/263A - Beautiful Matrix.cpp
+++ 800/263A - Beautiful Matrix.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() 
 {
+    //get a 5*5 matrix with 0s and one 1. Return the minimal number of neighbor rows/columns switch to get the 1 to the middle. 
+    //create matrix and get input for each place
     int matrix[5][5];
     for(int i=0;i<5;i++){
         for(int j=0;j<5;j++){
@@ -15,6 +17,7 @@ int main()
     int row = 0;
     int column = 0;
 
+    //get the position of the number 1
     for(int r = 0; r < 5; r++){
         for(int c = 0; c < 5; c++){
             if (matrix[r][c] == 1){
@@ -23,6 +26,8 @@ int main()
             }
         }
     }
+
+    //the minimal number of moves is the sum of the number of rows and number of columns that must be switched
     cout << abs(row - 2) + abs(column - 2); 
 
     return 0;
