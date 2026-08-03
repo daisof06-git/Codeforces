175fa9632073efa4284e51f31606a9b040537e3a
daisof06-git <daisof06@gmail.com>
2026-08-02 22:13:09 -0300

    Update 282A - Bit++.cpp

--- 800/282A - Bit++.cpp
+++ 800/282A - Bit++.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main() 
 {
+    //get n inputs with x++ or x-- or ++x or --x and return the value of x (initial is 0)
     int n; 
     cin >> n;
     int count = 0; 
     for(int i=0; i<n; i++){
         char a,b,c;
+
+        //get the input of each line
         cin >> a >> b >> c;
+
+        //check the middle char (it will always be + or -), then sum or extract
         if (b=='+'){
             count++;
         }
