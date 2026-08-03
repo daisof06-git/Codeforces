7fd57d12988ba7e68930d967cfaac1ab23b9b86b
daisof06-git <daisof06@gmail.com>
2026-08-02 21:08:24 -0300

    Update 41A - Watermelon.cpp

--- 800/41A - Watermelon.cpp
+++ 800/41A - Watermelon.cpp
@@ -5,7 +5,8 @@ int main()
 {
     int w;
     std::cin >> w;
- 
+
+    //use modulo to check whether it is a multiple (also, the number 2 can't be divided in two even numbers
     if (w % 2 == 1 || w == 2){
        std::cout << "NO\n";
     }
