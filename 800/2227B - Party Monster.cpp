f0b467634510ab9da6cebd0cc38ec0f42d57ddf9
daisof06-git <daisof06@gmail.com>
2026-08-02 22:20:51 -0300

    Update 2227B - Party Monster.cpp

--- 800/2227B - Party Monster.cpp
+++ 800/2227B - Party Monster.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() 
 {
+    //get lines with ( and ). See if switching the place of a substring ONCE converts it to a valid expression (equal num of ( and ) and opening and closing positions)
+    // t is the num of lines and n the number of ( and ) in each line. (there's one n before each line) 
     int t;
     cin >> t;
     char b = '(';
@@ -24,6 +26,8 @@ int main()
                 countc++;
             }
         }
+
+        // if a valid expression can be created, the count of ( minus the count of ) will be 0. 
         if ((countb - countc) < 0 || (countb - countc) > 0){
             cout << "NO" << "\n";
         }
